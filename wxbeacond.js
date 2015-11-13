65b45c8586f9ad694ab3b1272e182ceff88a2bd2
kob-to-wni <kob-to@wni.com>
2015-11-13 09:27:01 +0900

    WxBeaconのデータ処理を外部化

--- wxbeacond.js
+++ wxbeacond.js
@@ -2,6 +2,7 @@ var bleacon = require("bleacon");
 var moment = require("moment");
 var fs = require("fs");
 var path = require("path");
+var wxbeacon = require("./wxbeacon");
 
 /** バインド先IPアドレス */
 var BIND_ADDRESS = "127.0.0.1";
@@ -85,12 +86,10 @@ socket.on("connection", function(s){
  */
 bleacon.on('discover', function(beacon) {
 	//変換
-	var temperature = (((beacon.major >> 4) & 0x03ff) * 100 - 30000) / 1000.0;
-	var humidity = ((beacon.major << 3) & 0x0078) | ((beacon.minor >> 13) & 0x0007);
-	var pressure = Math.floor(((beacon.minor & 0x1fff) * 0.1 + 300) * 10) / 10;
+	var data = wxbeacon.parse(beacon);
 
 	//送信
-	socket.sockets.emit("data", '{ "rssi": ' + beacon.rssi + ', "temperature": ' + temperature + ', "humidity": ' + humidity + ', "pressure": ' + pressure + ' }');
+	socket.sockets.emit("data", data);
 });
 //ビーコン検索開始
-bleacon.startScanning("c722db4c5d911801beb5001c4de7b3fd");
+bleacon.startScanning(wxbeacon.UUID);
